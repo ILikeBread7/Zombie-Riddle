db12d72e50559d1501497213d36bf46bf12f4c57
pj <pjaworski47@gmail.com>
2015-03-13 21:51:23 +0100

    commit

--- game.js
+++ game.js
@@ -1,13 +1,36 @@
 var exitGame;
+var retryGame;
 
 function clickListener(event){
 	var rect=document.getElementById("editor_canv").getBoundingClientRect();
 	var x=Math.floor((event.pageX-rect.left)/40);	//+costam
 	var y=Math.floor((event.pageY-rect.top)/40); //+costam
+}
+
+function exit(){
+	$(".in_game").hide();
 	exitGame=true;
 }
 
-function checkForExit(interval,ctx,mapNumber){
+function retry(){
+	retryGame=true;
+}
+
+function disableAll(disable){
+	$("#bridges_game").attr("disabled",disable);
+	$("#switches_game").attr("disabled",disable);
+	$("#walls_game").attr("disabled",disable);
+	$("#thrower_game").attr("disabled",disable);
+	$("#fighter_game").attr("disabled",disable);
+	$("#nothing_game").attr("disabled",disable);
+}
+
+function actions(interval,ctx,mapNumber,map){
+	if(retryGame){
+		retryGame=false;
+		clearInterval(interval);
+		play(mapNumber,map);
+	}
 	if(exitGame){
 		clearInterval(interval);
 		if(mapNumber==-1){
@@ -27,22 +50,26 @@ function checkForExit(interval,ctx,mapNumber){
 	}
 }
 
-function play(mapNumber){
+function play(mapNumber,map){
 	exitGame=false;
-	var map,interval;
-	if(mapNumber==-1)
-		map=exportedMapCode();
-	if(mapNumber==-2)
-		map=$("#level_code").val();
-	if(mapNumber>=0)
-		map=readMap(mapNumber);
+	var interval;
+	if(map==undefined){
+		if(mapNumber==-1)
+			map=exportedMapCode();
+		if(mapNumber==-2)
+			map=$("#level_code").val();
+		if(mapNumber>=0)
+			map=readMap(mapNumber);
+	}
 	var canv=document.getElementById("canv");
 	var ctx=canv.getContext("2d");
-	fill(ctx,"#000000");
+	fill(ctx,"#ffffff");
 	$(".level_editor").hide();
 	$(".start").hide();
 	$("#volume_control").addClass("volume_control_in_level_editor");
 	$("#go_back").hide();
 	$("#signature").hide();
-	interval=setInterval(function(){checkForExit(interval,ctx,mapNumber);},20);
+	$(".in_game").show();
+	disableAll(false);
+	interval=setInterval(function(){actions(interval,ctx,mapNumber,map);},20);
 }
